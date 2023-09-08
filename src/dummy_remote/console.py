e87e3d79b0dace0b532102cd4bc447c76da15991
Sahil Dilwali <sahil.dilwali@bp.com>
2023-09-08 09:13:33 -0500

    Inital testing setup

--- src/dummy_remote/console.py
+++ src/dummy_remote/console.py
@@ -2,9 +2,9 @@ import textwrap
 
 import click
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 from . import __version__
-
 
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
@@ -13,6 +13,7 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 @click.version_option(version=__version__)
 def main():
     """The hypermodern Python project."""
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     with requests.get(API_URL, timeout=30, verify=False) as response:
         response.raise_for_status()
         data = response.json()
