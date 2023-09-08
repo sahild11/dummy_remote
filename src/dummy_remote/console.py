6944ca4090cbd65538a6ea754bbbff493255e14f
Sahil Dilwali <sahil.dilwali@bp.com>
2023-09-08 09:28:48 -0500

    Improved testing setup

--- src/dummy_remote/console.py
+++ src/dummy_remote/console.py
@@ -2,7 +2,7 @@ import textwrap
 
 import click
 import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.packages.urllib3.exceptions import InsecureRequestWarning # pylint: disable=import-error
 
 from . import __version__
 
@@ -13,7 +13,7 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 @click.version_option(version=__version__)
 def main():
     """The hypermodern Python project."""
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning) # pylint: disable=no-member
     with requests.get(API_URL, timeout=30, verify=False) as response:
         response.raise_for_status()
         data = response.json()
